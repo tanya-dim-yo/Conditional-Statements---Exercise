119a4e662c5aba3ad6a67ba69b4c532a1a5487a2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-28 04:33:02 +0300

    Add files via upload

---
+++ 02. Bonus Score/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialPoints = int.Parse(Console.ReadLine());
+            double bonusPoints = 0.0;
+            if (initialPoints <= 100)
+            {
+                bonusPoints += 5;
+            }
+            else if (initialPoints > 1000)
+            {
+                bonusPoints = initialPoints * 0.1;
+            }
+            else
+            {
+                bonusPoints = initialPoints * 0.2;
+            }
+            if (initialPoints % 2 == 0)
+            {
+                bonusPoints += 1;
+            }
+            else if (initialPoints % 10 == 5)
+            {
+                bonusPoints += 2;
+            }
+            Console.WriteLine(bonusPoints);
+            Console.WriteLine(initialPoints + bonusPoints);
+        }
+    }
+}
