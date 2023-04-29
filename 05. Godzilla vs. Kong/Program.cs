cd7f9c2d6154fd420872e0883155123ab215e4e7
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-29 04:05:45 +0300

    Add files via upload

---
+++ 05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int supers = int.Parse(Console.ReadLine());
+            double costumeCost = double.Parse(Console.ReadLine());
+            double decorSum = budget * 0.1; ;
+            double supersSum = supers * costumeCost;
+            double totalSum = decorSum + supersSum;
+            if (supers > 150)
+            {
+                supersSum -= supersSum * 0.1;
+                totalSum = decorSum + supersSum;
+            }
+            if (totalSum > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {totalSum - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - totalSum:f2} leva left.");
+            }
+        }
+    }
+}
