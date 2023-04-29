1c1c2e528f2e6a8be1cc4ff90eca0bc0c3cc2bdd
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-29 04:07:01 +0300

    Add files via upload

---
+++ 06. World Swimming Record/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecordSeconds = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double secondsPerMeter = double.Parse(Console.ReadLine());
+            double newSeconds = distance * secondsPerMeter;
+            double delay = Math.Floor(distance / 15.0) * 12.5;
+            double totalSeconds = newSeconds + delay;
+            if (totalSeconds < worldRecordSeconds)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalSeconds:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {totalSeconds - worldRecordSeconds:f2} seconds slower.");
+            }
+        }
+    }
+}
