dab85d78f34be87c3ad4c20285c9466193182c79
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-29 04:03:07 +0300

    Add files via upload

---
+++ 03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TimePlus15minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int minutesPlus = minutes + 15;
+            if (minutesPlus < 60)
+            {
+                if (minutesPlus < 10)
+                {
+                    Console.WriteLine($"{hour}:0{minutesPlus}");
+                }
+                else
+                {
+                    Console.WriteLine($"{hour}:{minutesPlus}");
+                }
+            }
+            else
+            {
+                hour += 1;
+                int newMinutes = minutesPlus - 60;
+                if (hour <= 23)
+                {
+                    if (newMinutes < 10)
+                    {
+                        Console.WriteLine($"{hour}:0{newMinutes}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hour}:{newMinutes}");
+                    }
+                }
+                else
+                {
+                    if (newMinutes < 10)
+                    {
+                        Console.WriteLine($"0:0{newMinutes}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"0:{newMinutes}");
+                    }
+                }
+            
+            }
+        }
+    }
+}
