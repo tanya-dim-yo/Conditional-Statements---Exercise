099685682717beb01e1403dab051c9162c0470d2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-04-30 03:47:21 +0300

    Add files via upload

---
+++ 08. Lunch Break/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LunchBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            int episodeDuration = int.Parse(Console.ReadLine());
+            int lunchbreakDuration = int.Parse(Console.ReadLine());
+            double lunchTime = lunchbreakDuration / 8.0;
+            double relaxTime = lunchbreakDuration / 4.0;
+            double watchingTime = lunchbreakDuration - (lunchTime + relaxTime);
+            if (watchingTime >= episodeDuration)
+            {
+                Console.WriteLine($"You have enough time to watch {movie} and left with {Math.Ceiling(watchingTime - episodeDuration)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {movie}, you need {Math.Ceiling(episodeDuration - watchingTime)} more minutes.");
+            }
+        }
+    }
+}
